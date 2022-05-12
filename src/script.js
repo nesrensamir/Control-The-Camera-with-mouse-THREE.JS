aa72680847818e389905dea2f1b68eb526cab227
Nesren Samir <86182948+nesrensamir@users.noreply.github.com>
2022-05-12 21:49:58 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,84 @@
+import './style.css'
+import * as THREE from 'three' 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+// Cursor  
+
+const cursor ={
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove',(event)=>{
+cursor.x=event.clientX /sizes.width - 0.5
+cursor.y= -(event.clientY /sizes.height - 0.5)
+})
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,0.1,100)
+//const aspectRatio = sizes.width /sizes.height
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)  
+
+//CONTROLS 
+const controls =new OrbitControls (camera, canvas) 
+controls.enableDamping= true
+//controls.target.y=0.25
+//controls.update()
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+  
+    // control camera by changing its position 
+    /*   
+    // update Camera  ** to see behind the cube we update x,z ** 
+    camera.position.x = Math.sin(cursor.x *Math.PI*2)*2
+    camera.position.z= Math.cos(cursor.x *Math.PI*2) *2
+    camera.position.y= cursor.y*5 
+    camera.lookAt(mesh.position)
+    */
+
+   // control camera by using buld in controls (Orbit Controls)
+     controls.update()
+     
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick() 
+
+
+
+
